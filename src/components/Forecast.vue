3e3d42cc0910558d1f7f195ec5bcf3d954217d09
Stephen Johnson-Tseu <71110517+stephjohnsons@users.noreply.github.com>
2023-12-22 10:25:10 +0800

    added icons

--- src/components/Forecast.vue
+++ src/components/Forecast.vue
@@ -6,11 +6,16 @@
     <div 
       v-for="(item, index) in props.forecastData" 
       :key="index" 
-      class="justify-items-center align-items-center p-2 rounded-md mx-2"
+      class="justify-center align-center p-2 rounded-md mx-2"
       :class="index === 0 ? 'bg-sky-600 bg-opacity-40 ms-0' : ''"
     >
       <p class="font-bold text-center">{{ item.day }}</p>
-      <img :src="getImgSrc(item.text)" :alt="`${item.text}`" />
+      <div class="max-w-16 max-h-16 mx-auto">
+        <img 
+          :src="getImgSrc(item.text)" 
+          :alt="`${item.text}`" 
+        />
+      </div>
       <p class="text-center">{{ item.text }}</p>
       <div class="flex flex-row space-x-2 font-light mx-auto justify-center">
         <p class="text-sky-600">L:{{ item.low }}º</p>
@@ -30,18 +35,7 @@ const props = defineProps({
   },
 })
 
-function weatherConditions(item) {
-  const conditionText = item.text;
-
-  if (conditionText === "Cloudy" || conditionText === "Showers" || conditionText === "Thunderstorms") {
-    return conditionText;
-  } else {
-    return "Sunny";
-  }
-}
-
 const getImgSrc = (item) => {
-  const condition = weatherConditions(item);
-  return `@/assets/conditions/${condition}.svg`;
+  return `src/assets/conditions/${item}.svg`;
 };
 </script>
