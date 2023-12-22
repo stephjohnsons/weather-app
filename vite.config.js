fc68f9efce2fc0cc1d8d6ec01d90f63cba4ece25
Stephen Johnson-Tseu <71110517+stephjohnsons@users.noreply.github.com>
2023-12-22 17:44:15 +0800

    updated vite file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { fileURLToPath, URL } from 'node:url'
+
+const base = process.env.NODE_ENV === 'production' ? '/weather-app' : '/';
 
 export default defineConfig({
   plugins: [
