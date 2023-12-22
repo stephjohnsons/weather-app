574243aebd5978e5e57e4870cf90a5bc820f0cc2
Stephen Johnson-Tseu <71110517+stephjohnsons@users.noreply.github.com>
2023-12-22 18:33:54 +0800

    removed vue from plugins

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   base: '/',
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
