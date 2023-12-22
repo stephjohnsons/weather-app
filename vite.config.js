eaceaa9d1d77eb176e54f354e916085c1d7bc2e8
Stephen Johnson-Tseu <71110517+stephjohnsons@users.noreply.github.com>
2023-12-22 18:05:48 +0800

    Fix main.js location and reference

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { fileURLToPath, URL } from 'node:url'
 
-const base = process.env.NODE_ENV === 'production' ? '/weather-app' : '/';
-
 export default defineConfig({
+  base: '/weather-app',
   plugins: [
     vue(),
     vueJsx(),
