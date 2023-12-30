b38b19c55a22be01debffa1e13b16202b0c10b2c
Stephen Johnson-Tseu <71110517+stephjohnsons@users.noreply.github.com>
2023-12-30 17:55:33 +0800

    rebased

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,85 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
-  <div class="bg-gradient-to-br from-sky-200 via-sky-400 to-sky-500 min-h-screen">
-    <header class="text-sky-900 flex flex-row">
-      <div>
-        <h1 class="text-2xl font-bold px-4 py-2">Weather App</h1>
-      </div>
-      <div class="ms-auto">
-        <Menu />
-      </div>
-    </header>
-    <footer class="mx-4 text-center">
-      <p class="text-sky-700 text-sm pb-4">2023 © Created by Stephen.</p>
-    </footer>
-  </div>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
 </template>
 
-<script setup>
-import { RouterView } from 'vue-router';
-import Menu from './components/Menu.vue';
-</script>
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'Home',
-      component: () => import('../views/Home.vue')
+      name: 'home',
+      component: HomeView
     },
     {
-      path: '/contact',
-      name: 'Contact',
-      component: () => import('../views/Contact.vue')
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
     }
   ]
 })
