4b16134908d2303728d7e5a9d768e67b8b2a1213
Stephen Johnson-Tseu <71110517+stephjohnsons@users.noreply.github.com>
2023-12-30 18:04:14 +0800

    rebased

--- src/App.vue
+++ src/App.vue
@@ -12,15 +12,6 @@ import HelloWorld from "./components/HelloWorld.vue";
       width="125"
       height="125"
     />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
   </header>
 
   <RouterView />
