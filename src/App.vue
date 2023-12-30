027a7ca3dafad463f7ca725b6a6c194c208e19f1
Stephen Johnson-Tseu <71110517+stephjohnsons@users.noreply.github.com>
2023-12-30 18:07:40 +0800

    rebased

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,40 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-  </header>
-
-  <RouterView />
+  <div class="bg-gradient-to-br from-sky-200 via-sky-400 to-sky-500 min-h-screen">
+    <header class="text-sky-900 flex flex-row">
+      <div>
+        <h1 class="text-2xl font-bold px-4 py-2">Weather App</h1>
+      </div>
+      <div class="ms-auto">
+        <div class="flex flex-row py-3 me-2">
+          <router-link
+            v-for="(item, index) in navigation"
+            :key="index"
+            :to="item.href"
+            class="px-3 py-2 rounded-md text-sm font-medium transition-all mx-1 text-sky-700 hover:bg-sky-300 hover:text-sky-800"
+            active-class="text-sky-900 bg-sky-200"
+          >
+            {{ item.name }}
+          </router-link>
+        </div>
+      </div>
+    </header>
+    <body class="pb-4">
+      <RouterView />
+    </body>
+    <footer class="mx-4 text-center">
+      <p class="text-sky-700 text-sm pb-4">2023 © Created by Stephen.</p>
+    </footer>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+import Menu from './components/Menu.vue';
+import Home from './views/Home.vue';
+import Contact from './views/Contact.vue';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+const navigation = [
+  { name: 'Weather', href: '/' },
+  { name: 'Contact', href: '/contact' },
+];
+</script>
