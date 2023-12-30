52fe8299dd34d258af9f2266bee8db19bdefa849
Stephen Johnson-Tseu <71110517+stephjohnsons@users.noreply.github.com>
2023-12-30 18:09:21 +0800

    rebased

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Inspect from "vite-plugin-inspect"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    Inspect()
   ],
   resolve: {
     alias: {
