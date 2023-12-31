ca6b6a76d30254dbce3989ee78256021149ff734
Stephen Johnson-Tseu <71110517+stephjohnsons@users.noreply.github.com>
2023-12-31 15:03:21 +0800

    refactored

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-gradient-to-br from-sky-200 via-sky-400 to-sky-500 min-h-screen">
     <header class="text-sky-900 flex flex-row">
-      <div>
+      <div class="flex items-center">
         <h1 class="text-2xl font-bold px-4 py-2">Weather App</h1>
       </div>
       <div class="ms-auto">
@@ -22,7 +22,14 @@
       <RouterView />
     </body>
     <footer class="mx-4 text-center">
-      <p class="text-sky-700 text-sm pb-4">2023 © Created by Stephen.</p>
+      <p class="text-sky-700 text-sm">
+        2023 © Created by 
+        <a href="stephjohnsons.com/page/" class="underline">Stephen.</a>
+      </p>
+      <p class="text-sky-600 text-sm pb-4">
+        Weather favicon by 
+        <a href="https://dribbble.com/laurareen" class="underline">Laura Reen.</a>
+      </p>
     </footer>
   </div>
 </template>
